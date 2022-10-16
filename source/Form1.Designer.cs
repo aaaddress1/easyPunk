d960cb749b3fffaf5ae988fb919c3bfd41ca60d1
ADR-WIN\aaaddress1 <aaaddress1@gmail.com>
2022-10-16 16:08:28 +0800

    no message

---
+++ source/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace easyPunk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.關閉EasyPunkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "尚未偵測到窗口";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "easyPunk";
+            this.notifyIcon1.BalloonTipTitle = "已經縮小到工具列 :)";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "CyberPunk 中文輸入法屏蔽";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.關閉EasyPunkToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(227, 36);
+            // 
+            // 關閉EasyPunkToolStripMenuItem
+            // 
+            this.關閉EasyPunkToolStripMenuItem.Name = "關閉EasyPunkToolStripMenuItem";
+            this.關閉EasyPunkToolStripMenuItem.Size = new System.Drawing.Size(226, 32);
+            this.關閉EasyPunkToolStripMenuItem.Text = "關閉 easyPunk";
+            this.關閉EasyPunkToolStripMenuItem.Click += new System.EventHandler(this.關閉EasyPunkToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 101);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CyberPunk 2077 - 自動屏蔽輸入法";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 關閉EasyPunkToolStripMenuItem;
+    }
+}
+
---
+++ source/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace easyPunk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool PostMessage(int hhwnd, uint msg, IntPtr wparam, IntPtr lparam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr LoadKeyboardLayout(string pwszKLID, uint Flags);
+
+
+        private static uint WM_INPUTLANGCHANGEREQUEST = 0x0050;
+        private static int HWND_BROADCAST = 0xffff;
+        private static string en_US = "00000409"; //英文
+        private static string cn_ZH = "00000804";
+        private static string zh_TW = "00000404";
+        private static uint KLF_ACTIVATE = 1;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            (new Thread(() =>
+            {
+                IntPtr lastCheckWindow = IntPtr.Zero;
+                for (; ; Thread.Sleep(300) )
+                {
+                    if (lastCheckWindow != GetForegroundWindow())
+                    {
+                        StringBuilder Buff = new StringBuilder(256);
+                        lastCheckWindow = GetForegroundWindow();
+                        GetWindowText(lastCheckWindow, Buff, 256);
+                  
+                        if (Buff.ToString().StartsWith("Cyberpunk 2077 (C)"))
+                            // disable IME
+                            PostMessage(HWND_BROADCAST, WM_INPUTLANGCHANGEREQUEST, IntPtr.Zero, LoadKeyboardLayout(en_US, KLF_ACTIVATE));
+                        else
+                            // recover IME (zh_tw)
+                            PostMessage(HWND_BROADCAST, WM_INPUTLANGCHANGEREQUEST, IntPtr.Zero, LoadKeyboardLayout(zh_TW, KLF_ACTIVATE));
+
+                        Invoke(new Action(() => { this.label1.Text = "當前掃描到視窗: " + Buff.ToString(); }));
+                    }
+   
+                }
+
+            })
+            { IsBackground = true }).Start();
+
+        }
+
+        private void 關閉EasyPunkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visible = false;
+            notifyIcon1.ShowBalloonTip(1000);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+        }
+    }
+}
