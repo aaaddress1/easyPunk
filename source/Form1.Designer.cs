0d6cfedbc62651c8263c4be693797a1c21dad283
Nesquate <nesquate.100001@gmail.com>
2022-10-18 23:51:59 +0800

    Add ComboBox to select which window should apply

--- source/Form1.Designer.cs
+++ source/Form1.Designer.cs
@@ -34,17 +34,24 @@
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.關閉EasyPunkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Location = new System.Drawing.Point(7, 77);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 21);
+            this.label1.Size = new System.Drawing.Size(89, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "尚未偵測到窗口";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // notifyIcon1
             // 
@@ -62,23 +69,76 @@
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.關閉EasyPunkToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(227, 36);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 26);
             // 
             // 關閉EasyPunkToolStripMenuItem
             // 
             this.關閉EasyPunkToolStripMenuItem.Name = "關閉EasyPunkToolStripMenuItem";
-            this.關閉EasyPunkToolStripMenuItem.Size = new System.Drawing.Size(226, 32);
+            this.關閉EasyPunkToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
             this.關閉EasyPunkToolStripMenuItem.Text = "關閉 easyPunk";
             this.關閉EasyPunkToolStripMenuItem.Click += new System.EventHandler(this.關閉EasyPunkToolStripMenuItem_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(9, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "欲隱藏輸入法的視窗：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "尚未選擇欲隱藏視窗";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "若未在上面選擇任何視窗，";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(201, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "預設判斷字串為 \"Cyberpunk 2077 (C)\"";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 101);
+            this.ClientSize = new System.Drawing.Size(342, 198);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "CyberPunk 2077 - 自動屏蔽輸入法";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -95,6 +155,11 @@
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 關閉EasyPunkToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- source/Form1.cs
+++ source/Form1.cs
@@ -20,6 +20,11 @@ namespace easyPunk
             InitializeComponent();
         }
 
+        public delegate bool WndEnumProc(
+           UInt32 hWnd,
+           IntPtr lParam
+        );
+
         [DllImport("user32.dll")]
         private static extern bool PostMessage(int hhwnd, uint msg, IntPtr wparam, IntPtr lparam);
 
@@ -40,29 +45,81 @@ namespace easyPunk
         [DllImport("user32.dll")]
         static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
 
+        [DllImport("user32.dll")]
+        static extern int GetWindowTextLengthW(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        private void refreshComboBoxItems()
+        {
+            Invoke(new Action(() => {
+                this.comboBox1.Items.Clear();
+
+
+                EnumWindows(delegate (IntPtr hwnd, IntPtr param) {
+                    StringBuilder Buff = new StringBuilder(256);
+                    if (IsWindowVisible(hwnd) && GetWindowTextLengthW(hwnd) != 0)
+                    {
+                        GetWindowText(hwnd, Buff, 256);
+                        this.comboBox1.Items.Add(Buff.ToString());
+                    }
+
+                    return true;
+                }, IntPtr.Zero);
+            }));
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
             (new Thread(() =>
             {
-                IntPtr lastCheckWindow = IntPtr.Zero;
-                for (; ; Thread.Sleep(300) )
-                {
-                    if (lastCheckWindow != GetForegroundWindow())
-                    {
-                        StringBuilder Buff = new StringBuilder(256);
-                        lastCheckWindow = GetForegroundWindow();
-                        GetWindowText(lastCheckWindow, Buff, 256);
-                  
-                        if (Buff.ToString().StartsWith("Cyberpunk 2077 (C)"))
-                            // disable IME
-                            PostMessage(HWND_BROADCAST, WM_INPUTLANGCHANGEREQUEST, IntPtr.Zero, LoadKeyboardLayout(en_US, KLF_ACTIVATE));
-                        else
-                            // recover IME (zh_tw)
-                            PostMessage(HWND_BROADCAST, WM_INPUTLANGCHANGEREQUEST, IntPtr.Zero, LoadKeyboardLayout(zh_TW, KLF_ACTIVATE));
+            IntPtr lastCheckWindow = IntPtr.Zero;
+            String comboBoxSelected = null;
 
-                        Invoke(new Action(() => { this.label1.Text = "當前掃描到視窗: " + Buff.ToString(); }));
+            refreshComboBoxItems();
+
+            for (; ; Thread.Sleep(300))
+            {
+                
+
+                Invoke(new Action(() => {
+                    if (comboBox1.Focused && comboBox1.GetItemText(comboBox1.SelectedItem) != "")
+                    {
+                        comboBoxSelected = comboBox1.GetItemText(comboBox1.SelectedItem);
+                        Invoke(new Action(() => { this.label3.Text = "當前匹配視窗字串：" + comboBoxSelected; }));
+                    }
+                }));
+
+
+                if (lastCheckWindow != GetForegroundWindow())
+                {
+                    refreshComboBoxItems();
+                    StringBuilder Buff = new StringBuilder(256);
+                    lastCheckWindow = GetForegroundWindow();
+                    GetWindowText(lastCheckWindow, Buff, 256);
+
+                    if (comboBoxSelected == "" || comboBoxSelected == null)
+                        comboBoxSelected = "Cyberpunk 2077 (C)";
+
+
+                    if (Buff.ToString().StartsWith(comboBoxSelected))
+                        // disable IME
+                        PostMessage(HWND_BROADCAST, WM_INPUTLANGCHANGEREQUEST, IntPtr.Zero, LoadKeyboardLayout(en_US, KLF_ACTIVATE));
+                    else
+                        // recover IME (zh_tw)
+                        PostMessage(HWND_BROADCAST, WM_INPUTLANGCHANGEREQUEST, IntPtr.Zero, LoadKeyboardLayout(zh_TW, KLF_ACTIVATE));
+
+                    Invoke(new Action(() => { this.label1.Text = "當前掃描到視窗: " + Buff.ToString(); }));
+
+                    Invoke(new Action(() => { this.label3.Text = "當前匹配視窗字串：" + comboBoxSelected; }));
                     }
    
                 }
@@ -87,6 +144,25 @@ namespace easyPunk
         private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             this.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
